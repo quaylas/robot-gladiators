00f1cbbb897710c943eb5ebd63dcd6ca241deecd
quaylas <quayla@cxadvisors.com>
2020-11-12 18:30:37 -0700

    added start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -61,15 +61,49 @@ var fight = function(enemyName) {
         }
     }
 }
-for(var i = 0; i < enemyNames.length; i++){
-    if (playerHealth > 0) {
-            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-            var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
-            fight(pickedEnemyName);
+//function to start a new game
+var startGame = function(){
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    // Fight all enemy robots
+    for(var i = 0; i < enemyNames.length; i++){
+        if (playerHealth > 0) {
+                window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+                var pickedEnemyName = enemyNames[i];
+                enemyHealth = 50;
+                fight(pickedEnemyName);
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    // at this point, the player is either out of health or out of enemies
+    endGame();
+    // play again!
+    startGame();
+}
+// end game
+var endGame = function(){
+    // if player is alive, they win
+    if (playerHealth > 0){
+        window.alert("You survived the game! You now have a score of " + playerMoney);
+    }
+    // if player is not alive ... well ...
+    else{
+        window.alert("You've lost your robot in battle!");
+    }
+    // invite players to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();
     }
     else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 }
+// start the game when the page loads
+startGame();
